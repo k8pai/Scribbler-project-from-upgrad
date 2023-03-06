7934f8d7ba34f35376b6442341731cbdd27feff9
k8pai <thek8pai@gmail.com>
2023-03-06 18:44:22 +0530

    updated postslist with metatags and passing data for editing page

--- scripts/postslist.js
+++ scripts/postslist.js
@@ -5,10 +5,36 @@ const confirmModal = document.getElementById('confirm-modal');
 
 postCards.forEach((postCard) => {
 	const deleteButton = postCard.querySelector('.fa-trash');
+	const editButton = postCard.querySelector('.fa-ellipsis');
 
 	deleteButton.addEventListener('click', () => {
 		confirmYesBtn.addEventListener('click', function () {
 			postCard.remove();
 		});
+	});
+	editButton.addEventListener('click', () => {
+		console.log('post card = ', postCard);
+		const name = postCard.querySelector('.post-name p').textContent;
+		console.log('name = ', name);
+		const title = postCard.querySelector('.post-heading-div p').textContent;
+		console.log('title = ', title);
+		const description =
+			postCard.querySelector('.post-description').textContent;
+		console.log('description = ', description);
+
+		const postID = postCard.dataset.postid;
+		var encodedName = encodeURIComponent(name);
+		var encodedTitle = encodeURIComponent(title);
+		var encodedDescription = encodeURIComponent(description);
+		// Redirect to the post details page with the post ID, title, and content as query parameters
+		window.location.href =
+			'post.html?id=' +
+			postID +
+			'&name=' +
+			encodedName +
+			'&title=' +
+			encodedTitle +
+			'&description=' +
+			encodedDescription;
 	});
 });
