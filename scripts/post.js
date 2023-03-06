3c23de324e25163a5378b0903bcef9754f343c2e
k8pai <thek8pai@gmail.com>
2023-03-06 22:23:44 +0530

    updated with fully working modal. modified warnings and errors

--- scripts/post.js
+++ scripts/post.js
@@ -1,30 +1,88 @@
 var queryParams = new URLSearchParams(window.location.search);
-console.log('on post page.');
+var likeCount = 0;
+var comments = [];
 
-const nameElement = document.getElementById('name');
+const nameElement = document.getElementById('author-name');
 const titleElement = document.getElementById('title');
 const descriptionElement = document.getElementById('description');
-
-console.log('name element = ', nameElement);
-console.log('title element = ', titleElement);
-console.log('description element = ', descriptionElement);
 
 var postId = queryParams.get('id');
 var postName = queryParams.get('name');
 var postTitle = queryParams.get('title');
 var postDescription = queryParams.get('description');
 
-console.log('post name  = ', postName);
-console.log('post title  = ', postTitle);
-console.log('post description  = ', postDescription);
-if (postName) {
-	nameElement.textContent = postName.trim();
-}
+// console.log('postname = ', postName);
+// console.log('postTitle = ', postTitle);
+// console.log('postDescription = ', postDescription);
 
+if (postName) {
+	nameElement.innerText = postName.trim();
+}
 if (postTitle) {
 	titleElement.innerText = postTitle.trim();
 }
-
 if (postDescription) {
 	descriptionElement.innerText = postDescription.trim();
 }
+
+// like logic with Event Listener
+const likeButton = document.getElementById('like-button');
+const likeContent = document.getElementById('like-counter');
+
+likeButton.addEventListener('click', () => {
+	likeCount++;
+
+	if (likeCount === 1) {
+		likeContent.innerText = `${likeCount} person likes this!`;
+		return;
+	}
+
+	likeContent.innerText = `${likeCount} people like this!`;
+});
+
+// comment logic with Event Listener and Arrays.
+const commentButton = document.getElementById('comment-button');
+const commentArea = document.getElementById('comment-area');
+const commentSection = document.getElementById('all-comments');
+
+commentButton.addEventListener('click', () => {
+	if (!commentArea.value) {
+		return;
+	}
+	const data = commentArea.value;
+	commentArea.value = '';
+	comments.unshift(data);
+	renderComments();
+});
+
+const renderComments = () => {
+	commentSection.innerHTML = comments
+		.map(
+			(data, ind) =>
+				`<li class="list-group-item" key=${ind}>${data}</li>`,
+		)
+		.join('');
+};
+
+// save/edit button toggle logic
+
+const editButton = document.getElementById('edit-button');
+const headingElement = document.querySelector('.title-div');
+editButton.addEventListener('click', () => {
+	const editingMode = editButton.classList.contains('editing');
+	if (editingMode) {
+		editButton.classList.remove('editing');
+		editButton.innerHTML = 'Edit <i class="fa-solid fa-pen-to-square"></i>';
+		titleElement.removeAttribute('contenteditable', true);
+		headingElement.style.border = 'none';
+		descriptionElement.removeAttribute('contenteditable', true);
+		descriptionElement.style.border = 'none';
+	} else {
+		editButton.classList.add('editing');
+		editButton.innerHTML = 'Save <i class="fa-solid fa-floppy-disk"></i>';
+		titleElement.setAttribute('contenteditable', true);
+		headingElement.style.border = '2px solid pink';
+		descriptionElement.setAttribute('contenteditable', true);
+		descriptionElement.style.border = '2px solid pink';
+	}
+});
